3b57f762ee00948451a00dfcff77dd5b13860a60
ysw <tkddnjs10@naver.com>
2024-04-25 12:33:32 +0900

    nav router

---
+++ auction_front/src/component/Auction/regist_form.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function regist_form() {
+    return (
+        <article>
+            <div>regist_form</div>    
+        </article>
+
+    );
+}
+export default regist_form;
--- auction_front/src/component/member/mypage.jsx
+++ auction_front/src/component/member/mypage.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
-function Home() {
+function mypage() {
     return (
+        
         <article>
-            <div>Home</div>    
+            <div>mypage</div>    
         </article>
     );
 }
-export default Home;
+export default mypage;
