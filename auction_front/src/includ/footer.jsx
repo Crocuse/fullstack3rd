3273728b6c45bfc2058b103c8cd4e76f6dd45c3e
ssn <snsong91@gmail.com>
2024-04-25 11:39:40 +0900

    헤더 네비 푸터

---
+++ auction_front/src/includ/footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <>
+            <footer >
+                <div class="footer_wrap">
+                    <div class="footer_info">
+                        <div class="footer_logo">
+
+                        </div>
+                        <div class="footer_company">
+                            <p>경기도 의정부시 의정부2동 센트럴 타워 442-12 6층 | 대표 : 비드버드</p>
+                            <p>개인정보보호책임자 : 비드버드 bidbird@example.com | 고객센터 : 031-1234-5678</p>
+                            <p>Copyright © 2024, 비드버드. All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </>
+    );
+}
+
+export default Footer;
