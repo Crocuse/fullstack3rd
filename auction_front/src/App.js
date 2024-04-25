12f8d19c0afd2a685b1cb57018533fc03744f141
ysw <tkddnjs10@naver.com>
2024-04-25 14:21:06 +0900

    fix upper

--- auction_front/src/App.js
+++ auction_front/src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 
 import './App.css';
-import Nav from './component/include/nav';
-import Footer from './component/include/footer';
-import Header from './component/include/header';
+import Nav from './component/include/Nav';
+import Footer from './component/include/Footer';
+import Header from './component/include/Header';
 import Home from './component/Home';
-import CstCT from './component/customer_center';
-import CurList from './component/Auction/current_list';
-import RegForm from './component/Auction/regist_form';
+import CstCT from './component/Customer_center';
+import CurList from './component/Auction/Current_list';
+import RegForm from './component/Auction/Regist_form';
 import PointAdd from './component/member/PointAddForm';
-import MyPage from './component/member/MyPage'
+import MyPage from './component/member/Mypage'
 import SignUp from './component/member/SignUpForm'
 import Login from './component/member/LoginForm'
 
@@ -24,13 +24,13 @@ function App() {
         <Nav />
         <Routes>
           <Route path='/' element={<Home/>}></Route>
-          <Route path='/auction/current_list' element={<CurList/>}></Route>
-          <Route path='/action/regist_form' element={<RegForm/>}></Route>
-          <Route path='/member/point_add_form' element={<PointAdd/>}></Route>
-          <Route path='/member/mypage' element={<MyPage/>}></Route>
-          <Route path='/customer_center' element={<CstCT/>}></Route>
-          <Route path='/member/signup_form' element={<SignUp/>}></Route>
-          <Route path='/member/login_form' element={<Login/>}></Route>
+          <Route path='/auction/Current_list' element={<CurList/>}></Route>
+          <Route path='/action/Regist_form' element={<RegForm/>}></Route>
+          <Route path='/member/Point_add_form' element={<PointAdd/>}></Route>
+          <Route path='/member/Mypage' element={<MyPage/>}></Route>
+          <Route path='/Customer_center' element={<CstCT/>}></Route>
+          <Route path='/member/Signup_form' element={<SignUp/>}></Route>
+          <Route path='/member/Login_form' element={<Login/>}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
--- auction_front/src/component/include/nav.jsx
+++ auction_front/src/component/include/nav.jsx
@@ -7,14 +7,14 @@ function Nav() {
             <ul>
                 <li>
                     <Link to="/">BidBird logo img</Link> &nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link to="/auction/current_list">진행중경매</Link> &nbsp;&nbsp;
-                    <Link to="/action/regist_form">경매등록</Link>&nbsp;&nbsp;
-                    <Link to="/member/point_add_form">포인트충전</Link>&nbsp;&nbsp;
-                    <Link to="/member/mypage">마이페이지</Link>&nbsp;&nbsp;
-                    <Link to="/customer_center">고객센터</Link>&nbsp;&nbsp;
+                    <Link to="/auction/Current_list">진행중경매</Link> &nbsp;&nbsp;
+                    <Link to="/action/Regist_form">경매등록</Link>&nbsp;&nbsp;
+                    <Link to="/member/Point_add_form">포인트충전</Link>&nbsp;&nbsp;
+                    <Link to="/member/Mypage">마이페이지</Link>&nbsp;&nbsp;
+                    <Link to="/Customer_center">고객센터</Link>&nbsp;&nbsp;
                     <Link >알람</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link to="/member/signup_form">회원가입</Link>&nbsp;&nbsp;
-                    <Link to="/member/login_form">로그인</Link>&nbsp;&nbsp;
+                    <Link to="/member/Signup_form">회원가입</Link>&nbsp;&nbsp;
+                    <Link to="/member/Login_form">로그인</Link>&nbsp;&nbsp;
                 </li>
 
             </ul>
