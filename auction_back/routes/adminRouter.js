ffa0e7b4efce8d4c0ae335d9e7cf6fd11680b500
BBangqu <bangjong21@naver.com>
2024-04-25 15:52:39 +0900

    어드민 오류수정

---
+++ auction_back/routes/adminRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const adminService = require('../lib/sevice/adminService');
+const router = express.Router();
+
+
+router.post('/admin_regist_confirm', (req, res) => {
+    console.log('/admin_regist_confirm');
+    adminService.adminRegistConfirm(req, res);
+
+});
+
+module.exports = router;
