820d80e4515fbf9bacf8961ce47b1a4f0f050604
bombombobm <studyjh951002@gmail.com>
2024-04-26 09:35:02 +0900

    merge

--- auction_back/routes/adminRouter.js
+++ auction_back/routes/adminRouter.js
@@ -1,9 +1,5 @@
 const express = require('express');
-<<<<<<< HEAD
-const adminService = require('../lib/sevice/adminService');
-=======
 const adminService = require('../lib/service/adminService');
->>>>>>> c57c2a621bb9a5afec868519eaf68ca9a70290e3
 const router = express.Router();
 
 
