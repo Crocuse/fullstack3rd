28b52548f8f51481c9ee7ec10877a97c27c6d9c6
ssn <snsong91@gmail.com>
2024-04-26 16:50:36 +0900

    본인인증API

--- auction_front/src/component/member/PointAddForm.jsx
+++ auction_front/src/component/member/PointAddForm.jsx
@@ -1,11 +1,40 @@
-import React from "react"
+import React, { useEffect } from "react";
 
 function PointAddForm() {
+    const IMP = window.IMP;
+
+    useEffect(() => {
+        IMP.init('imp55455045');
+
+    }, []);
+
+    function onClickCertification() {
+        IMP.certification({
+            pg: "inicis_unified",
+            merchant_uid: "store-9eef78c7-0222-4d91-9b80-8eccfdc3ff45",
+            popup: false,
+        });
+    }
+
     return (
-        
-        <article>
-            <div>point_add_form</div>    
-        </article>
+        <>
+            <article>
+                <div className="add_point_wrap">
+                    <div className="add_point_title">
+                        <p>포인트 충전</p>
+                    </div>
+                    <div className="add_point_content">
+                        <p>현재 포인트  : </p>
+                        <p>본인 인증 후 포인트 결제가 가능합니다.</p>
+                    </div>
+                    <button onClick={onClickCertification}>본인 인증</button>
+                </div>
+
+
+
+            </article >
+        </>
     );
 }
+
 export default PointAddForm;
