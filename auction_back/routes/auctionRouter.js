a487162f1228b5112048278d903c3c83f8f12884
ysw <tkddnjs10@naver.com>
2024-04-26 16:04:13 +0900

    img server

--- auction_back/routes/auctionRouter.js
+++ auction_back/routes/auctionRouter.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const auctionService = require('../lib/service/auctionService');
 const router = express.Router();
+const upload = require('../lib/config/uploads');
+
 
 
 router.post('/regist_form', (req, res) => {
     console.log('/auction/regist_form');
+    auctionService.goods_regist(req, res);
 });
 
 
