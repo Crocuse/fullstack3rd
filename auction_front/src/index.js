9b22180141b2d2fc36ab94fc4bb1cfe2a41d40cd
ssn <snsong91@gmail.com>
2024-04-26 17:41:08 +0900

    css 파일경로 수정 및 불필요파일 삭제

--- auction_front/src/index.js
+++ auction_front/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './static/css/common.css';
+import './css/common.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>
 );
 
