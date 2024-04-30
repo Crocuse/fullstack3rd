ebfce9af125ecf554bb3240a02182d82fb054609
ysw <tkddnjs10@naver.com>
2024-04-30 15:35:13 +0900

    auction reg

--- auction_back/lib/service/auctionService.js
+++ auction_back/lib/service/auctionService.js
@@ -4,25 +4,37 @@ const auctionService = {
     goods_regist : (req, res) => {
         let post = req.body;
         let files = req.files;
+        let mId = req.user;
 
-        // DB.query('INSERT INTO TBL_GOODS_REGIST(GR_NAME, GR_PRICE, GR_INFO, M_ID) VALUES(?, ?, ?, ?)', 
-        // [post.grName, post.grPrice, post.grInfo, 'id'],
-        // (error, result) => {
-        //     if(error){
-        //         console.log(error)
-        //     } else {
-        //         for(let i = 0; i < post.files.length; i++) {
-                    
-        //         }
-        //         //등록 넘버를 외래키로 테이블 하나 더 추가.
-        //         //실패하면 업로드 X
-        //         //성공하면 업로드 O
-        //         //이미지 업로드.
-        //     }
-        // })
+        console.log('mId', mId);
+
+        DB.query('INSERT INTO TBL_GOODS_REGIST(GR_NAME, GR_PRICE, GR_INFO, M_ID) VALUES(?, ?, ?, ?)', 
+        [post.grName, post.grPrice, post.grInfo, 'gildong'],
+        (error, result) => {
+            if(error){
+                console.log(error)
+                for(let i = 0; i < files.length; i++) {
+                    fs.unlink(`C:/ysw/${req.file.filename}`, (error) => {
+                        console.log('UPLOADED FILE DELETE COMPLETED!!');
+                    });
+                }
+                res.json('fail');
+            } else {              
+                let grNo = result.insertId;
+
+                for(let i = 0; i < files.length; i++) {
+                    DB.query('INSERT INTO TBL_GOODS_IMG(GI_NAME, GR_NO) VALUES(?, ?)',
+                    [files[i].filename,grNo],
+                    (error, result) => {
+                        if(error)
+                            console.log(error);
+                    });
+                }
+
+                res.json('success');
+            }
+        });
     }
-
-
 }
 
 module.exports = auctionService;
