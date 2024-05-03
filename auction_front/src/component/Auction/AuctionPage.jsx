be48bc95537761bd04f74d5e9ae819c3428f0db3
ysw <tkddnjs10@naver.com>
2024-05-03 16:36:21 +0900

    detailInfo

--- auction_front/src/component/Auction/AuctionPage.jsx
+++ auction_front/src/component/Auction/AuctionPage.jsx
@@ -3,8 +3,13 @@ import { sessionCheck } from "../../util/sessionCheck";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { SERVER_URL } from "../../config/server_url";
+import '../../css/Auction/AuctionPage.css';
+
+
+
 function AuctionPage() {
     const [asPrice, setAsPrice] = useState('');
+    const [idx, setIdx] = useState(0);
     const sessionId = useSelector(state => state['loginedInfos']['loginedId']['sessionId']);
     const navigate = useNavigate();
     const location = useLocation();
@@ -20,36 +25,65 @@ function AuctionPage() {
         setAsPrice(e.target.value); 
     }
 
-    return (
-        <div className="auction_page_wrap">
-            <div className="auction_title">{product.GR_NAME}</div>
-            <div className="auction_semi_info">
-                <div className="auction_img">
-                    <img src={`${SERVER_URL.SERVER_URL()}/goodsImg/${product.imgs[0]}`} alt="" />
-                </div>
-                <div className="auction_price">
-                    <h3>시작가 : {product.GR_PRICE}</h3>
-                    <h3>현재가 : {product.GR_PRICE}</h3>
-                </div>
-            </div>
-            <div className="auction_area">
-                <div className="auction_img">
-                    <img src="" alt="" />
-                </div>
-                <div className="auction_progress">
-                    <div className="auction_log">
+    const leftBtnClickHandler = () => {
+        console.log('leftBtnClickHandler()');
+        let tmp = idx;
+        tmp--;
+        if(tmp < 0) {
+            tmp = product.imgs.length -1;
+        }
+        setIdx(tmp);
+        console.log(tmp);
+    }
 
+    const rightBtnClickHandler = () => {
+        console.log('rightBtnClickHandler()');
+        let tmp = idx;
+        tmp++;
+        if(tmp > product.imgs.length -1) {
+            tmp = 0;
+        }
+        setIdx(tmp);
+        console.log(tmp);
+    }
+
+    return (
+        <article>
+            <div className="auction_page_wrap">
+                <div className="auction_semi_info">
+                    <div className="auction_back_img">
+                        <button onClick={leftBtnClickHandler}><img src="/img/arrow_left.png"/></button>
+                        <div className="auction_img">
+                            <img src={`${SERVER_URL.SERVER_URL()}/goodsImg/${product.imgs[idx]}`} alt="" />
+                        </div>
+                        <button><img src="/img/arrow_right.png" onClick={rightBtnClickHandler}/></button>
                     </div>
-                    <div className="auction_btn">
-                        <button>입찰</button><br/>
-                        <input type="number" name="as_price" onChange={(e) => asPriceChangeHandler(e)}/><button>호가 입찰</button>
+                    <div className="auction_info">
+                        <h1>{product.GR_NAME}</h1>
+                        <h2>시작가 {product.GR_PRICE}</h2>
+                        <h2>현재가 {product.GR_PRICE}</h2>
+                        <h4>판매자ID {product.M_ID}</h4>
+                        <h4>물건 소개 </h4>
+                        {product.GR_INFO}
+                    </div>
+                </div>
+                <div className="auction_area">
+                    <div className="auction_img">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="auction_progress">
+                        <div className="auction_log">
+
+                        </div>
+                        <div className="auction_btn">
+                            <button>입찰</button><br/>
+                            <input type="number" name="as_price" onChange={(e) => asPriceChangeHandler(e)}/><button>호가 입찰</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="auction_detail">
-                간단한 설명
-            </div>
-        </div>
+        </article>
+        
     );
 }
 export default AuctionPage;
