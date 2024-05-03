b2cb81441dc4f566e910c3da365c0d44caa49879
ysw <tkddnjs10@naver.com>
2024-05-03 14:40:14 +0900

    auction_list end

--- auction_front/src/component/Auction/AuctionPage.jsx
+++ auction_front/src/component/Auction/AuctionPage.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { sessionCheck } from "../../util/sessionCheck";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { SERVER_URL } from "../../config/server_url";
 function AuctionPage() {
     const [asPrice, setAsPrice] = useState('');
     const sessionId = useSelector(state => state['loginedInfos']['loginedId']['sessionId']);
     const navigate = useNavigate();
+    const location = useLocation();
+    const product = location.state.product;
+
     useEffect(() => {
         sessionCheck(sessionId, navigate);
         console.log(sessionId);
@@ -18,19 +22,19 @@ function AuctionPage() {
 
     return (
         <div className="auction_page_wrap">
-            <div className="auction_title">제품 이름</div>
+            <div className="auction_title">{product.GR_NAME}</div>
             <div className="auction_semi_info">
                 <div className="auction_img">
-                    <img src="#none"/>
+                    <img src={`${SERVER_URL.SERVER_URL()}/goodsImg/${product.imgs[0]}`} alt="" />
                 </div>
                 <div className="auction_price">
-                    <h3>시작가 : </h3>
-                    <h3>현재가 : </h3>
+                    <h3>시작가 : {product.GR_PRICE}</h3>
+                    <h3>현재가 : {product.GR_PRICE}</h3>
                 </div>
             </div>
             <div className="auction_area">
                 <div className="auction_img">
-                    <img src="#none"/>
+                    <img src="" alt="" />
                 </div>
                 <div className="auction_progress">
                     <div className="auction_log">
