cd9419efc379aa87c99af047c97dd24356afda7e
ssn <snsong91@gmail.com>
2024-05-07 20:26:15 +0900

    알림 수정

--- auction_back/lib/websocket/initializeSocket.js
+++ auction_back/lib/websocket/initializeSocket.js
@@ -15,22 +15,6 @@ function initializeSocket(server) {
         auctionAlarmHandler(socket, io);
 
     });
-
-
-    // io.on('connection', socket => {
-    //     console.log("websocket connected !!!! ");
-    //     socket.on('message', ({ name, message }) => {
-    //         io.emit('message', ({ name, message }));
-    //     });
-
-    //     socket.on('overbidding', ({ loginedId }) => {
-    //         console.log("loginedId====>", loginedId);
-    //         alarmService.getAcPointInfo(loginedId);
-
-    //     });
-    // });
-
-
 }
 
 module.exports = initializeSocket;
