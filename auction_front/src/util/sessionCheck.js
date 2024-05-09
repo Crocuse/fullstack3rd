9d8ae306e06f99ddc0def85d0b651aafaaf17648
bombombobm <studyjh951002@gmail.com>
2024-05-09 12:06:55 +0900

    풀~

--- auction_front/src/util/sessionCheck.js
+++ auction_front/src/util/sessionCheck.js
@@ -1,6 +1,5 @@
 export const sessionCheck = (sessionId, navigate) => {
-    // if (sessionId === '') {
-    //     navigate('/member/login_form');
-    // }
-    console.log('세션 체크는 잠시 꺼놔요~~~');
+    if (sessionId === '') {
+        navigate('/member/login_form');
+    }
 };
