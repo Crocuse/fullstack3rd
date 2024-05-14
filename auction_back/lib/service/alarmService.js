a94126eb1beed9fe89a238fef80601ffc6cc2ef3
suna <snsong91@gmail.com>
2024-05-15 05:06:56 +0900

    알람 클릭시 해당 상품 페이지 이동

--- auction_back/lib/service/alarmService.js
+++ auction_back/lib/service/alarmService.js
@@ -9,12 +9,12 @@ module.exports = {
             let grNo = socketData.grNo;
             let result = await AlarmDao.alarmOverBid(grNo);
             if (result && result.length > 0) {
-                io.emit('notificationOverBid', { message: '상회 입찰이 발생하였습니다.', id: result[0].M_ID, name: result[0].GR_NAME, date: result[0].highestBidDate });
+                io.emit('notificationOverBid', { message: '상회 입찰이 발생하였습니다.', id: result[0].M_ID, name: result[0].GR_NAME, date: result[0].highestBidDate, grNo: result[0].GR_NO });
                 socket.disconnect();
             }
 
         } catch (error) {
-            socket.emit("notificationOverBidErr", { message: "ERROR 관리자에 문의하세요. <br />고객센터 : 031-1234-5678", id: '', name: '', date: '' });
+            socket.emit("notificationOverBidErr", { message: "ERROR 관리자에 문의하세요. <br />고객센터 : 031-1234-5678", id: '', name: '', date: '', grNo: ''});
             socket.disconnect();
         }
     },
