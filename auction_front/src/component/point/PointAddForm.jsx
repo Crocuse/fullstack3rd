f2a21543e24441552e55c672648b2132802a7d5e
bombombobm <studyjh951002@gmail.com>
2024-05-16 11:39:43 +0900

    포인트 페이지 구분자 추가

--- auction_front/src/component/point/PointAddForm.jsx
+++ auction_front/src/component/point/PointAddForm.jsx
@@ -1,18 +1,18 @@
-import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
-import { SERVER_URL } from "../../config/server_url";
-import { useSelector } from "react-redux";
-import CertificationAPI from "./CertificationAPI";
-import PayAPI from "./PayAPI";
-import { useNavigate } from "react-router-dom";
-import { sessionCheck } from "../../util/sessionCheck"
-import "../../css/Point/point.css";
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
+import { SERVER_URL } from '../../config/server_url';
+import { useSelector } from 'react-redux';
+import CertificationAPI from './CertificationAPI';
+import PayAPI from './PayAPI';
+import { useNavigate } from 'react-router-dom';
+import { sessionCheck } from '../../util/sessionCheck';
+import '../../css/Point/point.css';
 
 function PointAddForm() {
-    const sessionId = useSelector(state => state['loginedInfos']['loginedId']['sessionId']);
+    const sessionId = useSelector((state) => state['loginedInfos']['loginedId']['sessionId']);
     const navigate = useNavigate();
 
-    const loginedId = useSelector(state => state.loginedInfos.loginedId.loginedId);
+    const loginedId = useSelector((state) => state.loginedInfos.loginedId.loginedId);
     const [chargeAmount, setChargeAmount] = useState('');
     const [currentPoint, setCurrentPoint] = useState('');
     const chargeInputRef = useRef(null);
@@ -23,12 +23,14 @@ function PointAddForm() {
     }, []);
 
     const chargeAmountHandler = (e) => {
-        setChargeAmount(e.target.value);
-    }
+        const inputValue = e.target.value.replace(/,/g, '');
+        const numberValue = Number(inputValue);
+        setChargeAmount(numberValue.toLocaleString());
+    };
 
     const resetChargeAmount = () => {
         setChargeAmount('');
-    }
+    };
 
     async function axios_get_my_point() {
         console.log('[POINT ADD FORM.JSX] axios_get_my_point()');
@@ -37,23 +39,21 @@ function PointAddForm() {
             const response = await axios.post(`${SERVER_URL.SERVER_URL()}/point/getMyPoint`, {
                 loginedId: loginedId,
             });
-            if (response.data === "err") {
+            if (response.data === 'err') {
                 setCurrentPoint('ERROR');
             }
 
-            if (response.data.message === "0") {
+            if (response.data.message === '0') {
                 setCurrentPoint('0');
             }
 
             if (response.data.currentPoint > 0) {
                 setCurrentPoint(response.data.currentPoint);
             }
-
         } catch (error) {
             console.log(error);
         }
     }
-
 
     return (
         <>
@@ -65,22 +65,38 @@ function PointAddForm() {
                     <div className="add_point_content">
                         <div>
                             <span className="span_txt">현재 포인트 : </span>
-                            <input type="text" className="poin_page_input" readOnly value={currentPoint} /><span className="span_txt">P</span> <br />
+                            <input
+                                type="text"
+                                className="poin_page_input"
+                                readOnly
+                                value={currentPoint.toLocaleString()}
+                            />
+                            <span className="span_txt">P</span> <br />
                         </div>
                         <div>
                             <span className="span_txt">포인트 충전 : </span>
-                            <input ref={chargeInputRef} type="text" className="poin_page_input" value={chargeAmount} onChange={(e) => chargeAmountHandler(e)} placeholder="포인트 충전 금액을 입력하세요." /><span className="span_txt">원</span> <br />
+                            <input
+                                ref={chargeInputRef}
+                                type="text"
+                                className="poin_page_input"
+                                value={chargeAmount}
+                                onChange={(e) => chargeAmountHandler(e)}
+                                placeholder="포인트 충전 금액을 입력하세요."
+                            />
+                            <span className="span_txt">원</span> <br />
                         </div>
                     </div>
                     {/* <CertificationAPI /> */}
                     <div className="point_btn_wrap">
-                        <PayAPI chargeAmount={chargeAmount} resetChargeAmount={resetChargeAmount} chargeInputRef={chargeInputRef} axios_get_my_point={axios_get_my_point} />
+                        <PayAPI
+                            chargeAmount={chargeAmount}
+                            resetChargeAmount={resetChargeAmount}
+                            chargeInputRef={chargeInputRef}
+                            axios_get_my_point={axios_get_my_point}
+                        />
                     </div>
                 </div>
-
-
-
-            </article >
+            </article>
         </>
     );
 }
