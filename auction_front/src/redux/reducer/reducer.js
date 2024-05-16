d5482d10e8d93c7154a55abcfda90f27bc1caad9
SONGSUNA <154863390+SONGSUNA@users.noreply.github.com>
2024-05-16 12:51:36 +0900

    알림 완성

--- auction_front/src/redux/reducer/reducer.js
+++ auction_front/src/redux/reducer/reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import { loginedInfoReducer } from "./loginedInfoReducer";
-import overBidReducer from './overBidReducer';
 import alarmInfoInDBReducer from './alarmInfoInDBReducer ';
 
 export const reducer = combineReducers({
     loginedInfos: loginedInfoReducer,
-    notificationOverBid: overBidReducer,
     alarmInfo: alarmInfoInDBReducer,
 });
