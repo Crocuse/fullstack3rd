dddcf998b75e923c933e430974817a25f8e432ec
ysw <tkddnjs10@naver.com>
2024-05-17 18:06:52 +0900

    drag fix

--- auction_front/src/component/Auction/RegistForm.jsx
+++ auction_front/src/component/Auction/RegistForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import '../../css/Auction/RegistForm.css'
+import '../../css/Auction/RegistForm.css';
 import axios from "axios";
 import { SERVER_URL } from "../../config/server_url";
 import { useNavigate } from "react-router-dom";
@@ -12,12 +12,8 @@ function RegistForm(props) {
     const [grInfo, setGrInfo] = useState('');
     const [isActive, setIsActive] = useState(false);
     const [img, setImg] = useState([]);
-
     const [files, setFiles] = useState([]);
-
-    const DragStartHandler = () => setIsActive(true);
-    const DragEndHandler = () => setIsActive(false);
-
+    
     const fileInputRef = useRef(null);
 
     const { modifyGoods, setShowModifyModal, isModify } = props;
@@ -27,9 +23,6 @@ function RegistForm(props) {
     
     useEffect(() => {
         sessionCheck(sessionId, navigate);
-        console.log(sessionId);
-        console.log(isModify);
-        console.log(modifyGoods);
         if (isModify && modifyGoods) {
             setGrName(modifyGoods.goods.GR_NAME);
             setGrPrice(modifyGoods.goods.GR_PRICE);
@@ -39,125 +32,99 @@ function RegistForm(props) {
             const initialImages = modifyGoods.images.map((image) => `C:\\acution\\goodsImg\\${image.GI_NAME}`);
             setImg(initialImages);
         }
-    },[isModify,modifyGoods])
+    }, [isModify, modifyGoods, sessionId, navigate]);
 
     const AuctionRegistBtnClickHandler = () => {
-        console.log('AuctionRegistBtnClickHandler()');
-
-        if(grName === ''){
+        if (grName === '') {
             alert('상품 이름을 입력 해주세요');
             return;
-        } else if(grPrice === ''){
+        } else if (grPrice === '') {
             alert('희망 가격을 입력 해주세요');
             return;
-        } else if(grInfo === ''){
+        } else if (grInfo === '') {
             alert('상품 설명을 입력 해주세요');
             return;
         }
 
         postTransferFile();
-        
     }
 
     async function postTransferFile() {
-        console.log('postTransferFile()');
-
-        let files = fileInputRef.current.files;
-        console.log(files);
-
         const formData = new FormData();
-        formData.append('grName', grName)
-        formData.append('grPrice', grPrice)
-        formData.append('grInfo', grInfo)
 
-        if(isModify) formData.append('grNo',modifyGoods.goods.GR_NO);
+        console.log(files);
+        formData.append('grName', grName);
+        formData.append('grPrice', grPrice);
+        formData.append('grInfo', grInfo);
 
-        for(let i = 0; i<files.length; i++){
-            console.log(files[i]);
+        if (isModify) formData.append('grNo', modifyGoods.goods.GR_NO);
+
+        for (let i = 0; i < files.length; i++) {
             formData.append('gr_imgs', files[i]);
         }
 
+        try {
+            const url = isModify 
+                ? `${SERVER_URL.SERVER_URL()}/auction/modify_goods_confirm`
+                : `${SERVER_URL.SERVER_URL()}/auction/regist_form`;
 
-
-        try{
-            if(isModify) {
-                const response = await axios.post(`${SERVER_URL.SERVER_URL()}/auction/modify_goods_confirm`, formData,{
-                    headers: {
-                        'Content-Type' : 'multipart/form-data'
-                    }
-                });
-    
-                if(response.data == 'success') {
-                    alert('수정이 완료 되었습니다.');
-                    props.setTemp(prev => !prev);
-                } else {
-                    alert('수정에 실패 했습니다.');
-                    
+            const response = await axios.post(url, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
                 }
+            });
+
+            if (response.data === 'success') {
+                alert(isModify ? '수정이 완료 되었습니다.' : '등록이 완료 되었습니다.');
+                if (!isModify) navigate('/');
+                else setShowModifyModal(false);
             } else {
-                const response = await axios.post(`${SERVER_URL.SERVER_URL()}/auction/regist_form`, formData,{
-                    headers: {
-                        'Content-Type' : 'multipart/form-data'
-                    }
-                });
-    
-                if(response.data == 'success') {
-                    alert('등록이 완료 되었습니다.');
-                    navigate('/');
-                } else {
-                    alert('등록에 실패 했습니다.');
-                    navigate('/auction/Regist_form');
-                }
+                alert(isModify ? '수정에 실패 했습니다.' : '등록에 실패 했습니다.');
+                if (!isModify) navigate('/auction/Regist_form');
             }
-        } catch(error) {
+        } catch (error) {
             console.log(error);
         }
     }
 
     const grNameChangeHandler = (e) => {
-        console.log('grNameChangeHandler()');
         setGrName(e.target.value); 
     }
 
     const grPriceChangeHandler = (e) => {
-        console.log('grPriceChangeHandler()');
         setGrPrice(e.target.value); 
     }
 
     const grInfoChangeHandler = (e) => {
-        console.log('grInfoChangeHandler()');
         setGrInfo(e.target.value); 
     }
 
     const DropHandler = (e) => {
-        console.log('DropHandler()');
         e.preventDefault();
         setIsActive(false);
         const newFiles = e.dataTransfer.files;
-        showAddImg(files);
+        showAddImg(newFiles);
         setFiles(prevFiles => [...prevFiles, ...newFiles]);
     }    
 
     const uploadChangeHandler = (e) => {
-        console.log('uploadHandler()');
         const newFiles = e.target.files;
-        showAddImg(files);
+        showAddImg(newFiles);
         setFiles(prevFiles => [...prevFiles, ...newFiles]);
     };
 
-    const showAddImg = (files) => {
-        console.log('showAddImg()');
-        if(img.length  + files.length > 5){
+    const showAddImg = (newFiles) => {
+        if (img.length + newFiles.length > 5) {
             alert('이미지는 5개 까지 올릴수 있습니다.');
             return;
         }
 
-        for(let i = 0; i < files.length; i++) {
+        for (let i = 0; i < newFiles.length; i++) {
             const reader = new FileReader();
             reader.onload = () => {
                 setImg(prevImg => [...prevImg, reader.result]);
             };
-            reader.readAsDataURL(files[i]);
+            reader.readAsDataURL(newFiles[i]);
         }
     }
 
@@ -166,27 +133,26 @@ function RegistForm(props) {
     }
 
     const deleteBtnHandler = (index, e) => {
-        console.log('deleteBtnHandler()');
-
         e.preventDefault();
-
         const tempImg = [...img];
+        const tempFiles = [...files];
         tempImg.splice(index, 1);
+        tempFiles.splice(index, 1);
         setImg(tempImg);
+        setFiles(tempFiles);
     }
 
     const AuctionModifyBtnClickHandler = async () => {
         await postTransferFile();
-        await setShowModifyModal(false);
-        
+        setShowModifyModal(false);
     }
 
-    const resetBtnClickHandler = () =>{
-        if(isModify == undefined){
-            setGrName('')
-            setGrPrice('')
-            setGrInfo('')
-        } else if(isModify == true){
+    const resetBtnClickHandler = () => {
+        if (!isModify) {
+            setGrName('');
+            setGrPrice('');
+            setGrInfo('');
+        } else if (isModify) {
             setGrName(modifyGoods.goods.GR_NAME);
             setGrPrice(modifyGoods.goods.GR_PRICE);
             setGrInfo(modifyGoods.goods.GR_INFO);
@@ -195,56 +161,54 @@ function RegistForm(props) {
 
     return (
         <article className="regist_form">
-            <h1>{isModify ?'상품 수정' :'상품 등록'}</h1>
+            <h1>{isModify ? '상품 수정' : '상품 등록'}</h1>
             <div>
                 <form>
-                <div className="regist_input_wrap">
-                    <span className="regist_text">상품 이름 </span>
-                    <input type="text" className="regist_input" name="gr_name" defaultValue={grName} onChange={(e) => grNameChangeHandler(e)}/> <br/><br/>
-                    <span className="regist_text">희망 가격 </span>
-                    <input type="number" className="regist_input" name="gr_price" defaultValue={grPrice} onChange={(e) => grPriceChangeHandler(e)}/> <br/><br/>
-                    <span className="regist_text">간단한 설명 </span>
-                    <input type="text" className="regist_input" name="gr_info" defaultValue={grInfo} onChange={(e) => grInfoChangeHandler(e)}/> <br/><br/>
-                </div>
-                <label
-                    className={`upload_area${isActive ? ' active' : ''}`}
-                    onDragEnter={DragStartHandler}
-                    onDragOver={DragOverHandler}
-                    onDragLeave={DragEndHandler}
-                    onDrop={DropHandler}
-                >
-                    <input type="file" name="gr_imgs" className="gr_img" onChange={(e) => uploadChangeHandler(e)} multiple ref={fileInputRef}/>
-                    {img.length !== 0 ? (
-                        img.map((image, index) => (
-                            <div key={index} className="img_angle">                                    
-                                <img className="add_img" src={image} /><br/>
-                                <button className="delete_btn" onClick={(e) => deleteBtnHandler(index, e)}><img src="/img/delete_FILL0_wght400_GRAD0_opsz24.png" /></button>      
-                            </div>
-                        ))
-                    ) : (
-                        <>
-                            <div className="img_text" >
+                    <div className="regist_input_wrap">
+                        <span className="regist_text">상품 이름 </span>
+                        <input type="text" className="regist_input" name="gr_name" value={grName} onChange={grNameChangeHandler}/> <br/><br/>
+                        <span className="regist_text">희망 가격 </span>
+                        <input type="number" className="regist_input" name="gr_price" value={grPrice} onChange={grPriceChangeHandler}/> <br/><br/>
+                        <span className="regist_text">간단한 설명 </span>
+                        <input type="text" className="regist_input" name="gr_info" value={grInfo} onChange={grInfoChangeHandler}/> <br/><br/>
+                    </div>
+                    <label
+                        className={`upload_area${isActive ? ' active' : ''}`}
+                        onDragOver={DragOverHandler}
+                        onDrop={DropHandler}
+                    >
+                        <input
+                            type="file"
+                            name="gr_imgs"
+                            className="gr_img"
+                            onChange={uploadChangeHandler}
+                            multiple
+                            ref={fileInputRef}
+                        />
+                        {img.length !== 0 ? (
+                            img.map((image, index) => (
+                                <div key={index} className="img_angle">                                    
+                                    <img className="add_img" src={image} alt="uploaded" /><br/>
+                                    <button className="delete_btn" onClick={(e) => deleteBtnHandler(index, e)}><img src="/img/delete_FILL0_wght400_GRAD0_opsz24.png" alt="delete" /></button>      
+                                </div>
+                            ))
+                        ) : (
+                            <div className="img_text">
                                 <p>클릭 혹은 파일을 이곳에 드롭 하세요.</p>
                                 <p>파일당 최대 3MB, 최대 5개</p>
                             </div>
-                            
-                        </>
-                    )}
-                </label>
-                <br/>
+                        )}
+                    </label>
+                    <br/>
                     <div className="regist_btn_wrap">
-                        <input className="regist_btn" type="button" value={isModify==true ?"Auction Modify" : "Auction Regist"} 
-                        onClick={isModify == true 
-                            ? 
-                          AuctionModifyBtnClickHandler 
-                        : AuctionRegistBtnClickHandler} />
+                        <input className="regist_btn" type="button" value={isModify ? "Auction Modify" : "Auction Regist"} 
+                        onClick={isModify ? AuctionModifyBtnClickHandler : AuctionRegistBtnClickHandler} />
                         <input className="regist_btn" type="reset" value="RESET" onClick={resetBtnClickHandler}/>
                     </div>    
                 </form>
             </div>    
         </article>
-
     );
 }
+
 export default RegistForm;
- 
