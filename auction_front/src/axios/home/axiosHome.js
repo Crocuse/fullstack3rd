745bd50d29be1b8725e48ac2ce267f5bd18fa373
SONGSUNA <154863390+SONGSUNA@users.noreply.github.com>
2024-05-17 17:00:26 +0900

    slick 수정

--- auction_front/src/axios/home/axiosHome.js
+++ auction_front/src/axios/home/axiosHome.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import { SERVER_URL } from '../../config/server_url';
 
-export const axiosBidImg = async (loginedId) => {
-    console.log('AXIOS BID IMG');
+export const axiosPreview = async () => {
+    console.log('AXIOS PREVIEW');
 
     try {
-        let result = await axios.get(`${SERVER_URL.SERVER_URL()}/home/bidImg`);
-        console.log(result);
+        let response = await axios.get(`${SERVER_URL.SERVER_URL()}/home/bidImg`);
+        console.log("여기로 오는지 ------>", response);
+        let result = response.data;
+        return result;
 
     } catch (error) {
+        console.log('AXIOS PREVIEW ERROR', error);
 
     }
 }
