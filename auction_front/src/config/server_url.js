91532b46a102a53b4948fc0640ff603330abb1dc
bombombobm <studyjh951002@gmail.com>
2024-05-20 17:49:51 +0900

    소켓 url 수정

--- auction_front/src/config/server_url.js
+++ auction_front/src/config/server_url.js
@@ -1,3 +1,4 @@
 export const SERVER_URL = {
     SERVER_URL: () => `${process.env.REACT_APP_API_URL + ':' + process.env.REACT_APP_PORT}`,
+    SOCKET_URL: () => `${process.env.REACT_APP_API_URL}`,
 };
