34d6da7646459b6145568bbd7717a8e7991a2be2
bombombobm <studyjh951002@gmail.com>
2024-05-21 11:54:16 +0900

    ip 수정

--- auction_back/lib/websocket/initializeSocket.js
+++ auction_back/lib/websocket/initializeSocket.js
@@ -3,28 +3,25 @@ const webSocketHandler = require('./webSocketHandler');
 const os = require('os');
 
 function initializeSocket(server) {
-
     let corsOrigin;
 
     if (os.version().includes('Windows')) {
-        corsOrigin = 'http://localhost:3000'
+        corsOrigin = 'http://localhost:3000';
     } else {
-        corsOrigin = 'http://3.24.176.186:3000'
+        corsOrigin = 'http://54.180.200.131:3000';
     }
 
     const io = socketIO(server, {
         cors: {
             origin: corsOrigin,
-            credentials: true
-        }
+            credentials: true,
+        },
     });
 
-    io.on('connection', socket => {
-        console.log("WEBSOCKET CONNECTED!! ");
+    io.on('connection', (socket) => {
+        console.log('WEBSOCKET CONNECTED!! ');
         webSocketHandler(socket, io);
     });
-
-
 }
 
 module.exports = initializeSocket;
